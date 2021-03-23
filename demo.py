da892d0f8f3a8c22cfe36efa32396b0a3c4a3a23
vaishnavi kanumalla <kanumallavaishnavi10@gmail.com>
2021-03-23 10:38:01 +0530

    yo babes

---
+++ demo.py
@@ -0,0 +1,15 @@
+fruits = ['apple', 'banana', 'cherry']
+fruits.append("orange")
+print(fruits)
+
+list = ['vaishnavi', 'kanumalla']
+list.insert(2,32)
+print(list)
+
+list1 = [1, 2, 3, 4, 5, 2]
+print(sum(list1))
+print(list1.count(2))
+print(len(list1))
+print(list1.index(2))
+print(min(list1))
+print(max(list1))
