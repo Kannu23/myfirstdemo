81699ee52b3f4697c581f98124b95e6264a9893b
vaishnavi kanumalla <kanumallavaishnavi10@gmail.com>
2021-04-03 22:00:36 +0530

    hi

---
+++ v.py
@@ -0,0 +1,9 @@
+vaish=[1, 2, 3, "aftaab", "localtrain", 100.00, 50.25]
+if 4 in vaish:
+    print("you got it babes")
+else:
+    print("nope")
+hassen=100
+vaishnavi=200
+if hassen is not vaishnavi:
+    print("no")
